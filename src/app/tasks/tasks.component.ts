c66d5f1364830a7d1796be4bf0d6168370ab5034
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-02-21 21:54:42 -0300

    uptading tasks when navigating back to TaskComponent

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { Task } from './shared/task.model';
 import { TaskService } from './shared/task.service';
+import { Page } from 'ui/page';
 
 import * as dialogs from 'ui/dialogs';
 
@@ -18,12 +19,17 @@ export class TasksComponent implements OnInit{
   public icons: Map<string, string> = new Map<string, string>();
   public newTask: Task;
 
-  public constructor(private taskService: TaskService, private router: Router){
+  public constructor(private taskService: TaskService, private router: Router, private page: Page){
     this.newTask = new Task(null, '');
     this.setIcons();
   }
 
   public ngOnInit(): void {
+    this.loadTasks();
+    this.page.on('navigatingTo', () => this.loadTasks());
+  }
+
+  private loadTasks() {
     this.taskService.getAll()
       .subscribe(
       tasks => this.tasks = tasks = tasks.sort((a, b) => b.id - a.id),
