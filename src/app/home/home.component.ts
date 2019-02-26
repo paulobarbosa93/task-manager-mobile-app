6baafabc00594582642e9a6736e5c1c1da64590d
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-02-25 23:55:16 -0300

    forcing redirect to login page after logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,7 @@ export class HomeComponent {
   public constructor(private router: Router, private authService: AuthService){ }
 
   public signOutUser(): void{
-    this.authService.signOut().subscribe(
-      () => this.router.navigate(['/sign-in'])
-    );
+    this.authService.signOut().subscribe();
+    this.router.navigate(['/sign-in']);
   }
 }
